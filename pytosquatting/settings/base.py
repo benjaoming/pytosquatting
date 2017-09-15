b279f72f1bf0f2d2c7a2ce05ceeb26d7f2db3fb0
Benjamin Bach <benjamin@overtag.dk>
2017-09-15 17:48:17 +0200

    Simplify aggregation to be eh nosql and just use caching

--- pytosquatting/settings/base.py
+++ pytosquatting/settings/base.py
@@ -119,3 +119,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
--- webpages/views.py
+++ webpages/views.py
@@ -1,5 +1,4 @@
-from datetime import timedelta
-from django.db.models import F, ExpressionWrapper, fields, Sum
+from django.db.models import Sum
 from django.views.generic.base import TemplateView
 
 from pingback import models
@@ -10,26 +9,30 @@ class Index(TemplateView):
     
     def get_context_data(self, **kwargs):
         c = TemplateView.get_context_data(self, **kwargs)
-        duration = ExpressionWrapper(F('last_seen')-F('first_seen'), output_field=fields.DurationField())
-        duration_float = ExpressionWrapper(duration, output_field=fields.FloatField())
-        count_per_duration = ExpressionWrapper(F('unique_count')/duration_float, output_field=fields.FloatField())
         top_pingbacks = models.Pingback.objects.filter(
             unique_count__gt=1,
-        ).annotate(
-            duration=duration,
-            timed_count=count_per_duration,
-        ).filter(duration__gt=timedelta(hours=2)).order_by('-timed_count')
+        ).values('package_name', 'unique_count', 'first_seen', 'last_seen')
         total_sum_per_hour = 0
         
-        top_20 = list(top_pingbacks[:20])
+        top_pingbacks = list(top_pingbacks)
         
-        for pkg in top_20:
-            if not pkg.duration:
+        top_pingbacks_static_refs = []
+        
+        for pkg in top_pingbacks:
+            
+            duration = pkg['last_seen'] - pkg['first_seen']
+
+            if not duration:
                 continue
-            avg_per_hour = pkg.unique_count / (pkg.duration.total_seconds()//3600)
-            pkg.avg_per_hour = avg_per_hour
-            pkg.avg_per_day = avg_per_hour * 24
+            
+            avg_per_hour = pkg['unique_count'] / (duration.total_seconds()//3600)
+            pkg['avg_per_hour'] = avg_per_hour
+            pkg['avg_per_day'] = avg_per_hour * 24
             total_sum_per_hour += avg_per_hour
+            top_pingbacks_static_refs.append({**pkg})
+        
+        sorted(top_pingbacks_static_refs, key=lambda x: x['avg_per_hour'])
+        top_20 = top_pingbacks_static_refs[:20]
         c['top'] = top_20
         c['total_unique_ips'] = models.Pingback.objects.all().aggregate(sum=Sum('unique_count'))['sum']
         return c
