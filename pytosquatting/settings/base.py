a2cdccdab49f21b5758b9d5c81eb081ebe069c4b
bbb <benjamin@overtag.dk>
2025-07-13 21:46:06 +0200

    Close #2 + upgrade Django

--- pytosquatting/settings/base.py
+++ pytosquatting/settings/base.py
@@ -112,6 +112,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
